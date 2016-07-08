c385c4ee99608726254b6f41a320ac6060db1977
Wallysson <wally.nm@gmail.com>
2016-07-08 15:49:53 -0300

    Removido pasta de project

--- projects/mario-project/templates/header.js
+++
@@ -1,9 +0,0 @@
-window.headerTemplate = `
-<div class="header-content">
-    <div class="header-content-inner" style=" text-align: center;">
-        <h1 style="font-size: 37.0833px;">Fique por dentro dos principais jogos do baixinho</h1>
-        <hr>
-        <p style="max-width: 800px; margin: 0 auto;">Descubra os principais títulos, curiosidades através dos anos, personagens que participaram das tramas e muito mais outras inormações interessantes sobre cada jogo, acesse os menus e conheça cada jogo!</p>
-    </div>
-</div>
-`;
